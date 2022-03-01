5b094181941e5f00e0da7d04a58f18473faf8d71
Igor Polousov <igorpolousov@gmail.com>
2022-03-01 12:59:25 +0300

    + test image struct for testing UI + test image array for Collection data source

--- ImageViewerTalantix/Model/API Model.swift
+++ ImageViewerTalantix/Model/API Model.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+// Структура для проверки работы интерфейса
+struct ImageTalantix {
+    var date: String
+    var image: UIImage
+}
+
+
+// Массив картинок для проверки работы интерфейса
+var images = [
+    ImageTalantix(date: "22.02.2022", image: UIImage.init(imageLiteralResourceName: "nssl0033")),
+    ImageTalantix(date: "15.06.2019", image: UIImage.init(imageLiteralResourceName: "nssl0034")),
+    ImageTalantix(date: "17.11.2018", image: UIImage.init(imageLiteralResourceName: "nssl0045")),
+    ImageTalantix(date: "19.08.2021", image: UIImage.init(imageLiteralResourceName: "nssl0091"))
+]
